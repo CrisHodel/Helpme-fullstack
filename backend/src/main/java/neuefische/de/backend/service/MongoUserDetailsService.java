74d6da8df8be7e37b3318def0e1d3939dfc53591
cristinahodel <cristina_hodel@yahoo.com>
2023-06-17 06:32:40 +0200

    The name of the class was changed

--- backend/src/main/java/neuefische/de/backend/service/MongoUserDetailsService.java
+++ backend/src/main/java/neuefische/de/backend/service/MongoUserDetailsService.java
@@ -2,9 +2,8 @@ package neuefische.de.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import neuefische.de.backend.model.UserDTO;
-import neuefische.de.backend.model.UserNoSave;
+import neuefische.de.backend.model.User;
 import neuefische.de.backend.repo.MongoUserRepo;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,33 +22,33 @@ public class MongoUserDetailsService implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
 
+
+    public UserDTO addUser(User userToAdd) {
+        userToAdd.setId(generateId.generateUId());
+        userToAdd.setPassword(passwordEncoder.encode(userToAdd.getPassword()));
+        userRepo.insert(userToAdd);
+        return new UserDTO(
+                userToAdd.getId(),
+                userToAdd.getName(),
+                userToAdd.getImg()
+        );
+    }
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        UserNoSave optionalUserNoSave = userRepo.findUserNoSaveByName(name)
+        User optionalUser = userRepo.findUserNoSaveByName(name)
                 .orElseThrow(() -> new UsernameNotFoundException("User with this username: " + name + "not found"));
-        return new User(optionalUserNoSave.getName(), optionalUserNoSave.getPassword(), List.of());
+        return new org.springframework.security.core.userdetails.User(optionalUser.getName(), optionalUser.getPassword(), List.of());
     }
 
-public UserDTO getUserPageById(String id){
-    Optional<UserNoSave> userNoSave = userRepo.findById(id);
+    public UserDTO getUserPageById(String id){
+    Optional<User> userNoSave = userRepo.findById(id);
     return userNoSave.get().convertUserToUserDTO();
 }
-public List<UserDTO> getAllUser(){
-        List<UserNoSave> userNoSaves = new ArrayList<>(userRepo.findAll());
+    public List<UserDTO> getAllUser(){
+        List<User> users = new ArrayList<>(userRepo.findAll());
         List<UserDTO> userDTOS = new ArrayList<>();
-        for(UserNoSave userNoSave : userNoSaves){
-            userDTOS.add(userNoSave.convertUserToUserDTO());
+        for(User user : users){
+            userDTOS.add(user.convertUserToUserDTO());
         }
         return userDTOS;
-    }
-
-    public UserDTO addUser(UserNoSave userNoSaveToAdd) {
-        userNoSaveToAdd.setId(generateId.generateUId());
-        userNoSaveToAdd.setPassword(passwordEncoder.encode(userNoSaveToAdd.getPassword()));
-        userRepo.insert(userNoSaveToAdd);
-        return new UserDTO(
-                userNoSaveToAdd.getId(),
-                userNoSaveToAdd.getName(),
-                userNoSaveToAdd.getImg()
-        );
     }
 }
