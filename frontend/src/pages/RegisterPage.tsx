63fcdc61fc0ccb4694ea4a31dbbb71b062adae73
cristinahodel <cristina_hodel@yahoo.com>
2023-06-17 06:33:59 +0200

    improved the visibility of buttons

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, {ChangeEvent, FormEvent, useState} from "react";
+import React, {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {Simulate} from "react-dom/test-utils";
 import "../css/registerPageCss/RegisterPage.css"
@@ -13,8 +13,7 @@ export default function RegisterPage(){
 
     const [password, setPassword] = useState<string>("");
 
-    function addUser(event: FormEvent<HTMLFormElement>){
-        event.preventDefault();
+    function addUser(){
         console.log(name)
         axios.post("/api/signUp/user", {
             name: name,
@@ -35,23 +34,28 @@ export default function RegisterPage(){
         setPassword(event.target.value)
     }
 
+    useEffect(addUser, [])
+
     return(
         <div className={"registerPage"}>
                 <form onSubmit={addUser}>
                     <div className={"registerPage"}>
                         <label htmlFor="Username"><b>Username</b></label>
-                            <input placeholder={"Username"} type="text" value={name} onChange={changeEventHandlerUserName}/></div>
+                            <input placeholder={"Username"} type="text" value={name} onChange={changeEventHandlerUserName}/>
+                    </div>
                     <div>
                         <label htmlFor="Password"><b>Password</b></label>
-                            <input placeholder={"Password"} type="password" value={name} onChange={changeEventHandlerUserPassword} required/>
+                            <input placeholder={"Password"} type="password" value={password} onChange={changeEventHandlerUserPassword}/>
                     </div>
                     <div>
                         <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                            <input placeholder={"Repeat Password"} type="password" value={name} onChange={changeEventHandlerUserPassword} required/>
+                            <input placeholder={"Repeat Password"} type="password" value={password} onChange={changeEventHandlerUserPassword}/>
                     </div>
+                    <div>
                         <label>
-                            <input type="checkbox" checked={true} name="remember" /> Remember me
+                            <input type="checkbox" defaultChecked name="remember" /> Remember me
                         </label>
+                    </div>
                     <div className="clearfix">
                         <button type="button" className="cancelbtn">Cancel</button>
                         <button type="submit" className="signupbtn">Sign Up</button>
