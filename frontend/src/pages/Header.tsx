9e7bd566e4f8205d105cd36afe06a7f0bf748a34
cristinahodel <cristina_hodel@yahoo.com>
2023-06-17 06:31:13 +0200

    new function onClickHomePage

--- frontend/src/pages/Header.tsx
+++ frontend/src/pages/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import "../css/headerPageCss/HeaderPage.css";
 
 export default function Header() {
 
@@ -13,13 +14,20 @@ export default function Header() {
         navigate("/signIn");
     }
 
+    function onClickHomePage() {
+        navigate("/home");
+    }
+
     return(
-        <form>
+        <form className={"header-container"}>
             <div>
-                <button onClick={onClickHandlerRegisterHandler}>Sign up</button>
+                <button className={"header-signup-button"} onClick={onClickHandlerRegisterHandler}>Sign up</button>
             </div>
             <div>
-                <button color={'primary'} onClick={onClickLoginRegisterHandler}>Sign in</button>
+                <button className={"header-signin-button"} onClick={onClickLoginRegisterHandler}>Sign in</button>
+            </div>
+            <div>
+            <button className="header-home-button" onClick={onClickHomePage}>Home</button>
             </div>
         </form>
     );
