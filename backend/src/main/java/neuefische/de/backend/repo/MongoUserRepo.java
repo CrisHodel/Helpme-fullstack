da7f423253debfe8363bdfed06334949e8d6eb34
cristinahodel <cristina_hodel@yahoo.com>
2023-06-17 06:33:14 +0200

    name changed

--- backend/src/main/java/neuefische/de/backend/repo/MongoUserRepo.java
+++ backend/src/main/java/neuefische/de/backend/repo/MongoUserRepo.java
@@ -1,12 +1,12 @@
 package neuefische.de.backend.repo;
 
-import neuefische.de.backend.model.UserNoSave;
+import neuefische.de.backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MongoUserRepo extends MongoRepository<UserNoSave, String > {
-    Optional <UserNoSave> findUserNoSaveByName(String name);
+public interface MongoUserRepo extends MongoRepository<User, String > {
+    Optional <User> findUserNoSaveByName(String name);
 }
