8f9116009957b78a72a3abf1e6b123be413e5225
cristinahodel <cristina_hodel@yahoo.com>
2023-06-19 18:35:11 +0200

    a new home-button added button gestylt

--- frontend/src/pages/Header.tsx
+++ frontend/src/pages/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
-import "../css/headerPageCss/HeaderPage.css";
+import "../css/headerPageCss/Header.css";
 
 export default function Header() {
 
