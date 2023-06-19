d4ecc1501c967bbefe627c02b366ab3e2dca6659
cristinahodel <cristina_hodel@yahoo.com>
2023-06-19 18:35:34 +0200

    click me button deleted

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import "../css/homePageCss/HomePage.css";
 
 export default function HomePage(){
 
+    const navigate = useNavigate();
+
+    const[inputValue, setInputValue] = useState("");
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+        setInputValue(event.target.value)
+    }
+    function handleSubmit(event: ChangeEvent<HTMLFormElement>){
+        console.log("Eingabe" + inputValue)
+        event.preventDefault()
+        setInputValue("");
+    }
 
     return(
-    <div>
-        <button>Click me</button>
-    </div>
+        <div className="topic-nav">
+            <div>
+            <button onClick={() => window.location.href = '#translate'}>Translate</button>
+            </div>
+            <div><button onClick={() => window.location.href = '#moving out'}>Moving out</button>
+            </div>
+            <div><button onClick={() => window.location.href = '#car repair'}>Car repairs</button>
+            </div>
+            <div><button onClick={() => window.location.href = '#other'}>Other</button>
+            </div>
+            <form onSubmit={handleSubmit} >
+                <input
+                    className={"input"}
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Schreibe etwas oder stelle eine Frage..."
+                />
+                <button type="submit">Senden</button>
+            </form>
+        </div>
     )
 }
