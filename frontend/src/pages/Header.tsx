3129f1db6366916b9957ca8935de04bd3bf67d35
cristinahodel <cristina_hodel@yahoo.com>
2023-06-22 16:56:11 +0200

    renamed the function

--- frontend/src/pages/Header.tsx
+++ frontend/src/pages/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
 import "../css/headerPageCss/Header.css";
+import Help_me from '../images/Help_me.png';
 
 export default function Header() {
 
     const navigate = useNavigate();
 
-    function onClickHandlerRegisterHandler() {
+    function onClickRegisterHandler() {
         navigate("/signUp");
     }
 
-    function onClickLoginRegisterHandler() {
+    function onClickLoginHandler() {
         navigate("/signIn");
     }
 
@@ -21,14 +22,20 @@ export default function Header() {
     return(
         <form className={"header-container"}>
             <div>
-                <button className={"header-signup-button"} onClick={onClickHandlerRegisterHandler}>Sign up</button>
+                <button className={"header-signup-button"} onClick={onClickRegisterHandler}>Sign up</button>
             </div>
             <div>
-                <button className={"header-signin-button"} onClick={onClickLoginRegisterHandler}>Sign in</button>
+                <button className={"header-signin-button"} onClick={onClickLoginHandler}>Sign in</button>
             </div>
             <div>
             <button className="header-home-button" onClick={onClickHomePage}>Home</button>
             </div>
+            <div>
+                <div>
+                    <img className={"logo"} src={Help_me} alt="logo"/>
+                    <p className={"logo-text"}>Help me</p>
+                </div>
+            </div>‚
         </form>
     );
 }
