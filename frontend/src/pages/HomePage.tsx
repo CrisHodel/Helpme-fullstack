9098a04abee99fa964294c7c8e908ab821307acc
cristinahodel <cristina_hodel@yahoo.com>
2023-06-22 16:56:35 +0200

    neue features are added

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -7,37 +7,64 @@ export default function HomePage(){
     const navigate = useNavigate();
 
     const[inputValue, setInputValue] = useState("");
+    const[answer, setAnswerValue] = useState("");
+    const[post, setPosts] = useState<string[]>([]);
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>){
         setInputValue(event.target.value)
     }
+
+    function handleAnswerChange(event: ChangeEvent<HTMLInputElement>){
+        setAnswerValue(event.target.value)
+    }
+
     function handleSubmit(event: ChangeEvent<HTMLFormElement>){
         console.log("Eingabe" + inputValue)
         event.preventDefault()
+        setPosts(prevPosts => [...prevPosts, inputValue]);
         setInputValue("");
     }
 
+    function handleSubmitAnswer(event: ChangeEvent<HTMLFormElement>){
+        console.log("Eingabe" + answer)
+        event.preventDefault()
+        setAnswerValue("");
+    }
+
     return(
-        <div className="topic-nav">
+        <div className="home-page">
+            <div className="topic-nav">
             <div>
-            <button onClick={() => window.location.href = '#translate'}>Translate</button>
+                <button onClick={() => window.location.href = '#translate'}>Translate</button>
             </div>
-            <div><button onClick={() => window.location.href = '#moving out'}>Moving out</button>
+            <div>
+                <button onClick={() => window.location.href = '#moving out'}>Moving out</button>
             </div>
-            <div><button onClick={() => window.location.href = '#car repair'}>Car repairs</button>
+            <div>
+                <button onClick={() => window.location.href = '#car repair'}>Car repairs</button>
             </div>
-            <div><button onClick={() => window.location.href = '#other'}>Other</button>
+            <div>
+                <button onClick={() => window.location.href = '#other'}>Other</button>
             </div>
-            <form onSubmit={handleSubmit} >
+            </div>
+            <form onSubmit={handleSubmit} className={"input-form"}>
                 <input
-                    className={"input"}
                     type="text"
                     value={inputValue}
                     onChange={handleInputChange}
+                    className={"input-field"}
                     placeholder="Schreibe etwas oder stelle eine Frage..."
                 />
-                <button type="submit">Senden</button>
+                <button type="submit" className={"submit-button"}>Post</button>
             </form>
+                <div className="posts">
+                    {post.map((post, index) => (
+                        <div key={index} className="post">
+                            {post}
+                        </div>
+                    ))}
+            < input type="text-answer" value={answer} onChange={handleAnswerChange} />
+        </div>
         </div>
     )
 }
